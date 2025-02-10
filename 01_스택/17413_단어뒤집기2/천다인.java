c62a09d6cd3fe13ea2abc0ce8f263c6d7973525a
dani <danii0110@naver.com>
2025-02-11 01:09:24 +0900

    02.10) 백준 17413 단어 뒤집기2

--- 01_스택/17413_단어뒤집기2/천다인.java
+++ 01_스택/17413_단어뒤집기2/천다인.java
@@ -1,26 +1,38 @@
-//언어 : JAVA , (성공/실패) : 1/4 , 메모리 : 365416 KB , 시간 : 2356 ms
-import java.util.*;
+//언어 : JAVA , (성공/실패) : 1/5 , 메모리 : 22936 KB , 시간 : 200 ms
 import java.io.*;
+import java.util.*;
 
-public class Main {
-
-    public static void main(String[] args) throws Exception {
+class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+        boolean inTag = false;
 
-        int N = Integer.parseInt(br.readLine());
-        int arr[] = new int[N];
+        String str = br.readLine();
 
-        for(int i=0; i<N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+        for (char c : str.toCharArray()) {
+            if (c == '<') {
+                while (!stack.isEmpty()) sb.append(stack.pop());
+
+                inTag = true;
+                sb.append(c);
+            } else if (c == '>') {
+                inTag = false;
+                sb.append(c);
+            } else if (inTag) {
+                sb.append(c);
+            } else {
+                if (c == ' ') {
+                    while (!stack.isEmpty()) sb.append(stack.pop());
+                    sb.append(c);
+                } else {
+                    stack.push(c);
+                }
+            }
         }
+        while (!stack.isEmpty()) sb.append(stack.pop());
 
-        Arrays.sort(arr); //배열 정렬함수 사용
-
-        for(int num : arr) {
-            sb.append(num).append('\n');
-        }
-
-        System.out.println(sb);
+        System.out.print(sb);
     }
 }
