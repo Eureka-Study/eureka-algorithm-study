286b1561cd9c9bacaa5f26c86356f5447cd95d4f
seungwoo505 <51819005+seungwoo505@users.noreply.github.com>
2025-02-11 20:04:39 +0900

    2일차 바이러스
    초반에 잘못생각한게 큰거같다... 시간이 생각보다 많이 걸림

---
+++ 02_BFS/2606_바이러스/Virus.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Virus {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int computer = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+
+        Queue<Integer> virus = new LinkedList<>();
+        boolean[] visited = new boolean[computer];
+        int count = 0;
+        virus.add(0);
+        visited[0] = true;
+
+        ArrayList<Integer>[] connect = new ArrayList[computer];
+        for(int i = 0; i < computer; i++){
+            connect[i] = new ArrayList<>();
+        }
+
+        for(int i = 0; i < N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+            int a = Integer.parseInt(st.nextToken())-1, b = Integer.parseInt(st.nextToken())-1;
+
+            connect[a].add(b);
+            connect[b].add(a);
+        }
+
+        while (!virus.isEmpty()) {
+            int v = virus.poll();
+
+            for(int i : connect[v]){
+                if (!visited[i]) {
+                    visited[i] = true;
+                    virus.add(i);
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
