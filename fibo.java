25fc49de8f843f8a0b07a6f1c8a31b9f032168cf
seungwoo505 <51819005+seungwoo505@users.noreply.github.com>
2025-02-13 21:11:04 +0900

    4일차 피보나치
    재귀로 풀려다가 for문으로 돌려서 풀었습니다.

---
+++ fibo.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class fibo {
+    static ArrayList<Integer[]> list = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        list.add(new Integer[]{1, 0});
+        list.add(new Integer[]{0, 1});
+
+        for(int i = 0; i < N; i++){
+            int n = Integer.parseInt(br.readLine());
+
+            for(int j = list.size(); j <= n; j++){
+                Integer[] one = list.get(j - 2);
+                Integer[] two = list.get(j - 1);
+
+                list.add(new Integer[]{one[0]+two[0], one[1]+two[1]});
+            }
+            Integer[] arr = list.get(n);
+            System.out.println(arr[0] + " " + arr[1]);
+        }
+    }
+}
