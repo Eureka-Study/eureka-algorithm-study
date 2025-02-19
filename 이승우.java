54f746ef6c16516ddb3e91423581e96fac6f6196
seungwoo505 <51819005+seungwoo505@users.noreply.github.com>
2025-02-19 16:27:16 +0900

    최소 힙 주석 추가

--- 이승우.java
+++ 이승우.java
@@ -10,19 +10,19 @@ public class heap{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        Queue<Integer> queue = new PriorityQueue<>();
-        StringBuilder sb = new StringBuilder();
+        Queue<Integer> queue = new PriorityQueue<>(); 
+        StringBuilder sb = new StringBuilder(); // 큐를 뺀 것을 저장
 
         for(int i = 0; i < N; i++){
             int x = Integer.parseInt(br.readLine());
 
-            if (x > 0) {
+            if (x > 0) { // 자연수일 경우
                 queue.add(x);
-            }else{
-                if(queue.isEmpty()){
-                    sb.append("0\n");
+            }else{ // 0일 경우
+                if(queue.isEmpty()){ // 큐가 비어있으면
+                    sb.append("0\n"); //0을 출력
                 }else{
-                    sb.append(queue.poll()).append("\n");
+                    sb.append(queue.poll()).append("\n"); //큐에서 빼고 그 값을 출력
                 }
             }
         }
