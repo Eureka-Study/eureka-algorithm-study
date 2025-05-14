de7ede742c1da6e2264f51dae0a499e155bb6b15
seungwoo <zoozoo1302@gmail.com>
2025-05-14 22:12:55 +0900

    주석 추가

--- 02_BFS/2206_벽 부수고 이동하기/이승우.java
+++ 02_BFS/2206_벽 부수고 이동하기/이승우.java
@@ -1,4 +1,4 @@
-//언어 : JAVA , (성공/실패) : 1/0 , 메모리 : 260012 KB , 시간 : 1648 ms
+//언어 : JAVA , (성공/실패) : 1/6 , 메모리 : 260012 KB , 시간 : 1648 ms
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -24,7 +24,7 @@ public class 벽부수고이동하기 {
         for (int n = 1; n <= N; n++) {
             String s = br.readLine();
             for (int m = 1; m <= M; m++) {
-                list[n][m] = s.charAt(m - 1) - '0';
+                list[n][m] = s.charAt(m - 1) - '0'; // 벽인지 길인지
             }
         }
 
@@ -32,35 +32,35 @@ public class 벽부수고이동하기 {
             if (n1[3] == n2[3])
                 return n1[2] - n2[2];
             return n1[3] - n2[3];
-        });
-        boolean[][][] visited = new boolean[2][N + 1][M + 1];
+        }); // 우선순위 큐 벽이 없는 구간 부터 탐색을 하고 같을 경우 이동한 거리가 짧은 순으로 탐색
+        boolean[][][] visited = new boolean[2][N + 1][M + 1]; // 벽일 때랑 벽이 아닐 때랑 구분하여 탐색을 한다.
         int min = Integer.MAX_VALUE;
 
-        queue.offer(new int[] { 1, 1, 1, 0 });
+        queue.offer(new int[] { 1, 1, 1, 0 }); // 초기는 1, 1 이며 처음 위치도 이동 거리에 포함이므로 1, 벽을 부순 적이 없기에 0
 
         while (!queue.isEmpty()) {
             int[] q = queue.poll();
             if (visited[q[3]][q[0]][q[1]])
                 continue;
-            if (q[0] == N && q[1] == M) {
+            if (q[0] == N && q[1] == M) { // 최종 위치에 도달했을 경우
                 min = Math.min(q[2], min);
-                continue;
+                continue; // 다른 도달 가능성이 있기 때문에 true로 바뀌기 전에 넘기기
             }
             visited[q[3]][q[0]][q[1]] = true;
 
             for (int[] yx : d) {
                 int[] newPoint = new int[] { q[0] + yx[0], q[1] + yx[1], q[2] + 1, q[3] };
 
-                if (newPoint[0] > 0 && newPoint[0] <= N &&
-                        newPoint[1] > 0 && newPoint[1] <= M &&
-                        !(newPoint[3] == 1 && list[newPoint[0]][newPoint[1]] == 1)) {
-                    newPoint[3] = newPoint[3] == 0 ? list[newPoint[0]][newPoint[1]] : 1;
-                    if (!visited[newPoint[3]][newPoint[0]][newPoint[1]]) {
+                if (newPoint[0] > 0 && newPoint[0] <= N && // 배열 안에 존재해야함
+                        newPoint[1] > 0 && newPoint[1] <= M && // 배열 안에 존재해야함
+                        !(newPoint[3] == 1 && list[newPoint[0]][newPoint[1]] == 1)) { // 벽을 이미 부셨을 경우 벽을 만나면 무조건 못지나간다.
+                    newPoint[3] = newPoint[3] == 0 ? list[newPoint[0]][newPoint[1]] : 1; // 벽을 부셨는지 체크
+                    if (!visited[newPoint[3]][newPoint[0]][newPoint[1]]) { // 아직 안지나갔다면
                         queue.offer(newPoint);
                     }
                 }
             }
         }
-        System.out.println(min == Integer.MAX_VALUE ? -1 : min);
+        System.out.println(min == Integer.MAX_VALUE ? -1 : min); // int 최대값일 경우 n, m에 도달한 적이 없다는 의미이므로 -1이다.
     }
 }
--- 22_조합/n과m(2)/이승우.java
+++ 22_조합/n과m(2)/이승우.java
@@ -17,19 +17,19 @@ public class NM2 {
         M = Integer.parseInt(st.nextToken());
         sb = new StringBuilder();
 
-        num("", 1, 0); // n = 1
+        num("", 1, 0);
 
         System.out.println(sb);
     }
 
     static void num(String s, int n, int m) {
-        if (m == M) {
+        if (m == M) { // 조합 개수가 충족했는지 확인
             sb.append(s.trim())
                     .append("\n");
             return;
         }
         for (int i = n; i <= N; i++) {
-            num(s + i + " ", i + 1, m + 1);
+            num(s + i + " ", i + 1, m + 1); // 특정 조합을 지우는 것은 아니기 때문에 if문 없이 모든 구간을 재귀 해본다.
         }
     }
 }
